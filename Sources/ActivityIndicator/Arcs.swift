18c9be4731fba9f39257d4f62a2736fb806a02bf
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-29 19:14:07 +0330

    Rotating Arcs Added.

---
+++ Sources/ActivityIndicator/Arcs.swift
@@ -0,0 +1,54 @@
+//
+//  Arcs.swift
+//  SwiftUIPlayground
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 9/27/20.
+//  Copyright © 2020 Chenzook. All rights reserved.
+//
+//  StackOverflow: https://stackoverflow.com/story/mojtabahosseini
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//
+
+import SwiftUI
+
+public struct Arcs: View {
+    @Binding private var isAnimating: Bool
+    public let count: UInt
+    public let width: CGFloat
+    public let spacing: CGFloat
+
+    public init(animate: Binding<Bool>, count: UInt = 3, width: CGFloat = 2, spacing: CGFloat = 1) {
+        self._isAnimating = animate
+        self.count = count
+        self.width = width
+        self.spacing = spacing
+    }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            ForEach(0..<Int(count)) { index in
+                item(forIndex: index, in: geometry.size)
+                    .rotationEffect(isAnimating ? .degrees(360) : .degrees(0))
+                    .animation(
+                        Animation.default
+                            .speed(Double.random(in: 0.2...0.5))
+                            .repeatCount(isAnimating ? .max : 1, autoreverses: false)
+                    )
+            }
+        }
+    }
+
+    private func item(forIndex index: Int, in geometrySize: CGSize) -> some View {
+        Group { () -> Path in
+            var p = Path()
+            p.addArc(center: CGPoint(x: geometrySize.width/2, y: geometrySize.height/2),
+                     radius: geometrySize.width/2 - width/2 - CGFloat(index) * (width + spacing),
+                     startAngle: .degrees(0),
+                     endAngle: .degrees(Double(Int.random(in: 120...300))),
+                     clockwise: true)
+            return p.strokedPath(.init(lineWidth: width))
+        }
+        .frame(width: geometrySize.width, height: geometrySize.height)
+    }
+}
