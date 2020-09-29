5ec7a786da917ff6144e35fc57f611a2e3ec1e9f
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-09-29 19:17:14 +0330

    Rotating Shapes added.

---
+++ Sources/ActivityIndicator/RotatingShapes.swift
@@ -0,0 +1,54 @@
+//
+//  RotatingShapes.swift
+//  SwiftUIPlayground
+//
+//  Created by Seyed Mojtaba Hosseini Zeidabadi on 9/27/20.
+//  Copyright © 2020 Chenzook. All rights reserved.
+//
+//  StackOverflow: https://stackoverflow.com/story/mojtabahosseini
+//  Linkedin: https://linkedin.com/in/MojtabaHosseini
+//  GitHub: https://github.com/MojtabaHs
+//
+
+import SwiftUI
+
+public struct RotatingShapes: View {
+    @Binding private var isAnimating: Bool
+    public let count: UInt
+    public let content: AnyView
+
+    public init<Content: View>(animate: Binding<Bool>, count: UInt = 6, content: () -> Content) {
+        self._isAnimating = animate
+        self.count = count
+        self.content = AnyView(content())
+    }
+
+    public init(animate: Binding<Bool>, count: UInt = 6, size: CGFloat = 20, spacing: CGFloat = 1) {
+        self.init(animate: animate, count: count) {
+            Circle().frame(width: size, height: size)
+        }
+    }
+
+    private func animatingScale(forIndex index: Int) -> CGFloat { CGFloat(index+1)/CGFloat(count) }
+
+    public var body: some View {
+        GeometryReader { geometry in
+            ForEach(0..<Int(count)) { index in
+                item(forIndex: index, in: geometry.size)
+                    .rotationEffect(isAnimating ? .degrees(360) : .degrees(0))
+                    .animation(
+                        Animation
+                            .timingCurve(0.5, 0.15 + Double(index) / 5, 0.25, 1, duration: 1.5)
+                            .repeatCount(isAnimating ? .max : 1, autoreverses: false)
+                    )
+                    .frame(width: geometry.size.width, height: geometry.size.height)
+            }
+        }
+    }
+
+    private func item(forIndex index: Int, in geometrySize: CGSize) -> some View {
+        content
+            .scaleEffect(isAnimating ? animatingScale(forIndex: index) : 1 )
+            .offset(y: geometrySize.width/10 - geometrySize.height/2)
+    }
+}
