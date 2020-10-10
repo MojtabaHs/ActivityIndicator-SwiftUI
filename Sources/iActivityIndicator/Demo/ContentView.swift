051ba2db67f019af4b0600c01ba2ca991092daae
Seyed Mojtaba Hosseini Zeidabadi <chenzook@gmail.com>
2020-10-11 02:20:32 +0330

    Embeded demo files updated.

--- Sources/iActivityIndicator/Demo/ContentView.swift
+++ Sources/iActivityIndicator/Demo/ContentView.swift
@@ -17,39 +17,39 @@ struct UsageView: View {
         VStack(spacing: 24) {
 
             HStack(spacing: 24) {
-                ActivityIndicator(style: .arcs())
-                ActivityIndicator(style: .arcs(width: 8))
-                ActivityIndicator(style: .arcs(count: 10))
+                iActivityIndicator(style: .arcs())
+                iActivityIndicator(style: .arcs(width: 8))
+                iActivityIndicator(style: .arcs(count: 10))
             }
 
             HStack(spacing: 24) {
-                ActivityIndicator(style: .bars(opacityRange: 1...1))
-                ActivityIndicator(style: .bars(scaleRange: 1...1))
-                ActivityIndicator(style: .bars(count: 3))
+                iActivityIndicator(style: .bars(opacityRange: 1...1))
+                iActivityIndicator(style: .bars(scaleRange: 1...1))
+                iActivityIndicator(style: .bars(count: 3))
             }
 
             HStack(spacing: 24) {
-                ActivityIndicator(style: .blinking())
-                ActivityIndicator(style: .blinking(count: 4))
-                ActivityIndicator(style: .blinking(count: 3, size: 50))
+                iActivityIndicator(style: .blinking())
+                iActivityIndicator(style: .blinking(count: 4))
+                iActivityIndicator(style: .blinking(count: 3, size: 50))
             }
 
             HStack(spacing: 24) {
-                ActivityIndicator() // The Default
-                ActivityIndicator(style: .classic(count: 13, width: 2))
-                ActivityIndicator(style: .classic(count: 3, width: 50))
+                iActivityIndicator() // The Default
+                iActivityIndicator(style: .classic(count: 13, width: 2))
+                iActivityIndicator(style: .classic(count: 3, width: 50))
             }
 
             HStack(spacing: 24) {
-                ActivityIndicator(style: .rotatingShapes())
-                ActivityIndicator(style: .rotatingShapes(count: 3, size: 30))
-                ActivityIndicator(style: .rotatingShapes(content: AnyView(Text("🎃").fixedSize())))
+                iActivityIndicator(style: .rotatingShapes())
+                iActivityIndicator(style: .rotatingShapes(count: 3, size: 30))
+                iActivityIndicator(style: .rotatingShapes(content: AnyView(Text("🎃").fixedSize())))
             }
 
             HStack(alignment: .center, spacing: 24) {
-                ActivityIndicator(style: .rowOfShapes())
-                ActivityIndicator(style: .rowOfShapes(count: 1, opacityRange: 0...1))
-                ActivityIndicator(style: .rowOfShapes(count: 3, scaleRange: 0.1...1))
+                iActivityIndicator(style: .rowOfShapes())
+                iActivityIndicator(style: .rowOfShapes(count: 1, opacityRange: 0...1))
+                iActivityIndicator(style: .rowOfShapes(count: 3, scaleRange: 0.1...1))
             }
         }
         .padding()
